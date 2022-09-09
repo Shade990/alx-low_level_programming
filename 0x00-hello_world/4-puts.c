d1c7b1339b7cf2824bfcc119e58930dd79ed60c6
Shade990 <folasademorine199029@yahoo.com>
2022-09-09 08:36:49 -0700

    task 4 5 6 100

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+
+/**
+ *main - Printing the string
+ *
+ * Returnc Always 0 (successful)
+ */
+int main(void)
+{
+	puts("\"programming is like building a multilingual puzzle");return (0);
+}
