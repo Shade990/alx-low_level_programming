5b1cff1cb01e09487b69bd0ba6fce2a26141df63
Shade990 <folasademorine199029@yahoo.com>
2022-09-12 02:51:41 -0700

    positve or negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
-#include <time.h>
 #include <stdlib.h>
-
+#include <time.h>
 /**
- * main - point of contact
- * Description: Prints if number is positive, zero or negative
- * Return: 0 (Success)
+ * main - Entry level point
+ *
+ * Return: Always 0
  */
 int main(void)
 {
 	int n;
 
-	
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	
 	if (n > 0)
-	{
-		Printf("%d is positive\n",);
-	}
-	else if (n == 0)
-	{
-		printf("%d is zero\n", n);
-	}
-	else
-	{
+		printf("%d is positive\n", n);
+	else if (n < 0)
 		printf("%d is negative\n", n);
-	}
+	else
+		printf("%d is zero\n", n);
 	return (0);
 }
